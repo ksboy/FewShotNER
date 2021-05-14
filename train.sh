04289746bb4db2f7227b10a8b5973a18fb5a4fcb
ksboy <weihouks@gmail.com>
2021-05-14 21:02:50 +0800

    update

--- train.sh
+++ train.sh
@@ -8,14 +8,14 @@ mkdir -p $OUTPUT_DIR
 # CUDA_VISIBLE_DEVICES=0 nohup python -m debugpy --listen 0.0.0.0:8888 --wait-for-client train_demo.py \
 CUDA_VISIBLE_DEVICES=0 nohup python -u train_demo.py  \
 --model $MODEL \
---train data/structdata/conll-2003/train.txt \
---val data/structdata/conll-2003/dev.txt \
+--train data/structdata/ontonotes-5.0/train.txt \
+--val data/structdata/ontonotes-5.0/dev.txt \
 --test data/structdata/wnut/test.txt \
 --test_support data/structdata/support-wnut-5shot/0.txt \
 --test_query data/structdata/wnut/test.txt \
 --support_fixed \
 --lr 2e-5 --batch_size 1 \
---trainN 4 --evalN 4 --N 6 --K 5 --Q 5 \
+--trainN 18 --evalN 18 --N 6 --K 5 --Q 5 \
 --fp16 \
 --train_iter 30000 \
 --val_iter 1000 \
@@ -27,3 +27,18 @@ CUDA_VISIBLE_DEVICES=0 nohup python -u train_demo.py  \
 # CUDA_VISIBLE_DEVICES=4,5 nohup python -u train_demo.py  --train data/mydata/train-intra.txt --val data/mydata/val-intra.txt --test data/mydata/test-intra.txt --lr 1e-4 --batch_size 4 --trainN 5 --N 5 --K 5 --Q 5 --train_iter 30000 --val_iter 1000 --test_iter 5000 --val_step 2000 --max_length 100 > trainlog/mydata-intra-5-5.log &
 # --only_test \
 # --load_ckpt ./checkpoint/proto-train.txt-dev.txt-6-5.pth.tar \
+
+
+
+# CUDA_VISIBLE_DEVICES=0 nohup python -u train_demo.py  \
+# --model structshot \
+# --train data/structdata/ontonotes-5.0/train.txt \
+# --val data/structdata/ontonotes-5.0/dev.txt \
+# --test data/structdata/test-wnut.txt \
+# --lr 2e-5 --batch_size 1 \
+# --trainN 4 --evalN 4 --N 6 --K 5 --Q 5 \
+# --train_iter 30000 \
+# --val_iter 1000 \
+# --test_iter 100 \
+# --val_step 1000 \
+# --max_length 128 > trainlog/structdata-structshot-4-6-5-5.log 2>&1 &
